bb9226ff5b1e982c71f2e622d87b67efc72bca52
Vermilion15 <seigneur.noir15@gmail.com>
2023-11-08 00:21:20 +0700

    add: server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const express = require('express')
+const path = require("path");
+const app = express()
+
+// #############################################################################
+// This configures static hosting for files in /public that have the extensions
+// listed in the array.
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+app.use(express.static('build', options))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`React app listening at http://localhost:${port}`)
+})
